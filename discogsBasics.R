6c70ddacf06dec65ef230b894291fbdeb425483e
Ben <bac3917@gmail.com>
2019-01-22 12:54:29 -0500

    Update discogsBasics.R

--- discogsBasics.R
+++ discogsBasics.R
@@ -1,6 +1,4 @@
 # The Artist resource represents a person in the Discogs database who contributed to a Release in some capacity.
 library(jsonlite); library(DT);library(magrittr);library(dplyr);library(ggplot2)
-
 MONKreleases<-fromJSON("https://api.discogs.com/artists/145256/releases")        
-MONKreleases
 plot(MONKreleases$releases$year)
